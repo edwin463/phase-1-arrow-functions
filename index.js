14e4cd19e7310e24bc03052fa181f3365c8153e4
edwin karuri njoroge <edwin.njoroge1@student.moringaschool.com>
2024-09-25 06:00:59 -0700

    arrows

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1) Function expression called divide
+const divide = function() {
+    return 2000 / 100; // 2) divides 2000 by 100
+  }
+  
+  // 3) Arrow function called square
+  const square = (x) => x * x; // 4) takes one parameter and multiplies it by itself
+  
+  // 5) Arrow function called add
+  const add = (a, b) => a + b; // 6) takes two parameters and adds them together
+  
